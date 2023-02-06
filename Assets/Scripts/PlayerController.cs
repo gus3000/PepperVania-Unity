8965672c5688b7d143f230bfa85e3ccd09954a69
gus3000 <gus3000spam@gmail.com>
2023-02-06 22:36:56 +0100

    feat: implement collision on map border

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ using UnityEngine.Serialization;
 public class PlayerController : MonoBehaviour
 {
     private const string DashAnimationName = "PepperArmature|Dash";
-
+    
     [SerializeField, Tooltip("in m/s")] private float speed = 2;
     [SerializeField] private float rotationSpeed = 10f;
     [SerializeField] private float dashDurationMultiplier = 1f;
@@ -26,6 +26,8 @@ public class PlayerController : MonoBehaviour
     private PlayerInput _playerInput;
     private IsoFollow _camera;
     private GameController _gameController;
+    private Rigidbody _rigidbody;
+    private CapsuleCollider _playerCollider;
 
     // the SerializeField below this are for debug only
     [SerializeField] private Vector3 _velocity;
@@ -48,17 +50,22 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        _velocity = Vector3.forward;
         _playerInput = GetComponent<PlayerInput>();
         _velocity = Vector3.zero;
         _camera = GameObject.FindWithTag("MainCamera").GetComponent<IsoFollow>();
         _animator = GetComponentInChildren<Animator>();
         _inputMovement = Vector2.zero;
         _gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _playerCollider = GetComponent<CapsuleCollider>();
+
         var dashAnimation = _animator.runtimeAnimatorController.animationClips.First(clip => clip.name == DashAnimationName);
         _baseDashDuration = dashAnimation.length;
         HasMoved = false;
+
+        Debug.Log($"clamped to 1 : {Vector3.ClampMagnitude(Vector3.forward, 1)}");
+        Debug.Log($"clamped to 0 : {Vector3.ClampMagnitude(Vector3.forward, 0)}");
+        Debug.Log($"clamped to -1 : {Vector3.ClampMagnitude(Vector3.forward, -1)}");
     }
 
 
@@ -92,7 +99,96 @@ public class PlayerController : MonoBehaviour
         if (_isDashing)
             velocityModifier *= dashSpeedBoost;
 
-        transform.Translate(_velocity * (speed * Time.deltaTime * velocityModifier), Space.World);
+        _rigidbody.velocity = _velocity * (speed * velocityModifier);
+        // Move(_velocity * (speed * Time.deltaTime * velocityModifier));
+        // transform.Translate(_velocity * (speed * Time.deltaTime * velocityModifier), Space.World);
+    }
+
+    void Move(Vector3 movement)
+    {
+        var capsDistance = _playerCollider.height / 2f;
+        var center = _playerCollider.bounds.center;
+        var tip = center + transform.forward * capsDistance;
+        if (UnityEngine.Physics.Raycast(tip, movement, out var raycastHit, movement.magnitude,
+                LayerMask.GetMask("Obstacle"), QueryTriggerInteraction.Ignore))
+        {
+            // Debug.Log($"raycast hit at {raycastHit.distance} with normal {raycastHit.normal}");
+            // Debug.Log($"magnitude {movement.magnitude} -> {raycastHit.distance} => {Vector3.ClampMagnitude(movement, raycastHit.distance).magnitude}");
+            // Debug.Log($"reducing movement from {movement} to {Vector3.ClampMagnitude(movement, raycastHit.distance)}");
+            // var maxMagnitude = raycastHit.distance - capsDistance - epsilon;
+            // if (maxMagnitude < 0)
+            //     maxMagnitude = 0;
+            // movement = Vector3.ClampMagnitude(movement, maxMagnitude); //easy mode
+            // movement = FixMovement(movement, tip, raycastHit) * .9f;
+            // var fixedMovement = FixMovement(movement, tip, raycastHit);
+        }
+
+        // transform.Translate(movement, Space.World);
+        _rigidbody.MovePosition(transform.position + movement);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (_playerCollider == null)
+            return;
+        // return;
+        // var movement = transform.forward;
+        var capsDistance = _playerCollider.height / 2f;
+        var movement = _velocity * (speed * Time.deltaTime);
+        var center = _playerCollider.bounds.center;
+        var tip = center + transform.forward * capsDistance;
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawCube(center, Vector3.one * 0.05f);
+        Gizmos.DrawSphere(tip, .05f);
+        Gizmos.DrawRay(center, transform.forward * capsDistance);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawRay(tip, movement);
+
+        if (UnityEngine.Physics.Raycast(tip, movement, out var raycastHit, movement.magnitude,
+                LayerMask.GetMask("Obstacle"), QueryTriggerInteraction.Ignore))
+        {
+            Gizmos.color = Color.gray;
+            Gizmos.DrawSphere(raycastHit.point, .1f);
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(raycastHit.point, raycastHit.normal);
+
+            //fixmovement
+            // var correctMovement = FixMovement(movement, raycastHit);
+            var baseMove = raycastHit.point - tip;
+            var restMove = movement - baseMove;
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawRay(tip, baseMove);
+
+            // Gizmos.color = Color.blue;
+            // Gizmos.DrawRay(raycastHit.point, restMove);
+            // Debug.Log($"base : {baseMove}, rest : {restMove}");
+
+            var restMoveFixed = Vector3.ProjectOnPlane(restMove, raycastHit.normal);
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawRay(tip + baseMove, restMoveFixed);
+
+            var correctMovement = baseMove + restMoveFixed;
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(tip, correctMovement);
+        }
+    }
+    private Vector3 FixMovement(Vector3 movement, Vector3 tip, RaycastHit raycastHit)
+    {
+        var minimumDistanceFromWall = 0.1f;
+        var baseMove = raycastHit.point - tip;
+        var restMove = movement - baseMove;
+        var restMoveFixed = Vector3.ProjectOnPlane(restMove, raycastHit.normal);
+        var correctMovement = baseMove + restMoveFixed + (raycastHit.normal * minimumDistanceFromWall);
+        // if ((tip + correctMovement - raycastHit.point).magnitude < minimumDistanceFromWall)
+        // {
+            // correctMovement += raycastHit.normal * minimumDistanceFromWall;
+            // correctMovement *= (correctMovement.magnitude - minimumDistanceFromWall) / correctMovement.magnitude;
+        // }
+        return correctMovement;
+        
     }
 
     void HandleInput()
